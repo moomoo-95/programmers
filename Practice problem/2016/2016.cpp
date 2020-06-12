8f67457aaf3041eb21d938138453eb2ce45166f3
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-12 23:24:15 +0900

    Create 2016.cpp

---
+++ Practice problem/2016/2016.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+	vector<int> month = { 0, 31, 60, 91, 121, 152, 182,
+		213, 244, 274, 305, 335 };
+	vector<string> day = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
+	string answer = day[(month[a - 1] + b) % 7];
+	return answer;
+}
