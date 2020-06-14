f662f45821779d41752506dd358011c2985b768a
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-15 00:09:12 +0900

    Update continuous duplicate deletion.cpp

--- Practice problem/continuous duplicate deletion/continuous duplicate deletion.cpp
+++ Practice problem/continuous duplicate deletion/continuous duplicate deletion.cpp
@@ -1,8 +1,17 @@
-#include <string>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
-string solution(string s) {
-	if (s.size() % 2 == 1) { return s.substr(s.size() / 2, 1); }
-	else { return s.substr((s.size() - 1) / 2, 2); }
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	answer.push_back(arr[0]);
+	for (int i = 1; i < arr.size(); i++) {
+		if (answer[answer.size() - 1] != arr[i]) {
+			answer.push_back(arr[i]);
+		}
+	}
+
+	return answer;
 }
