bb7c7a1f6fef641a1b1224ca7212c6575cacfaa0
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-18 00:05:52 +0900

    Create Multiple.cs

---
+++ Practice problem/Multiple/Multiple.cs
@@ -0,0 +1,26 @@
+// 연습문제 나누어 떨어지는 숫자 배열 Level 1
+
+using System.Collections;
+public class Solution
+{
+    public int[] solution(int[] arr, int divisor)
+    {
+        ArrayList answer = new ArrayList();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % divisor == 0)
+            {
+                answer.Add(arr[i]);
+            }
+        }
+        if (answer.Count == 0)
+        {
+            answer.Add(-1);
+        }
+        else
+        {
+            answer.Sort();
+        }
+        return (int[])answer.ToArray(typeof(int));
+    }
+}
