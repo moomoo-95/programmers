69bf1a12e93b5831b0766feb87aea8445fc59f28
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-18 21:31:33 +0900

    Create Find Kim.cpp

---
+++ Practice problem/Find Kim/Find Kim.cpp
@@ -0,0 +1,15 @@
+﻿// 연습문제 서울에서 김서방 찾기 Level 1
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<string> seoul) {
+    vector<string>::iterator iter;
+    iter = find(seoul.begin(), seoul.end(), "Kim");
+    string answer ="김서방은 ";
+    answer += to_string(iter - seoul.begin());
+    answer += "에 있다";
+    return answer;
+}
