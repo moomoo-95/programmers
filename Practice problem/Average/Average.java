e83b8b3de900cfde320ebe2de7d826d175ecd298
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-22 22:59:37 +0900

    Update Average.java

--- Practice problem/Average/Average.java
+++ Practice problem/Average/Average.java
@@ -1,10 +1,11 @@
-// 연습문제 2016 Level 1
+// 연습문제 평균 구하기 Level 1
 
 class Solution {
-    public String solution(int a, int b) {
-        int[] month = {0, 31, 60, 91, 121, 152, 182,213, 244, 274, 305, 335};
-        String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-        String answer = day[(month[a-1] + b) % 7];
-        return answer;
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i = 0; i < arr.length; i++){
+            answer += arr[i];
+        }
+        return answer / arr.length;
     }
 }
