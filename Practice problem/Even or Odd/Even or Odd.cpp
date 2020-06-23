a5f8e8f913a3c675f16546b3885df6d199d4c05b
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-23 22:28:06 +0900

    Create Even or Odd.cpp

---
+++ Practice problem/Even or Odd/Even or Odd.cpp
@@ -0,0 +1,19 @@
+// 연습문제 같은 숫자는 싫어 Level 1
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> answer;
+	answer.push_back(arr[0]);
+	for (int i = 1; i < arr.size(); i++) {
+		if (answer[answer.size() - 1] != arr[i]) {
+			answer.push_back(arr[i]);
+		}
+	}
+
+	return answer;
+}
