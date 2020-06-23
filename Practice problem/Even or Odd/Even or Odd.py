52da3abeed2f25f4c06211dda4a6263c3f6b3a51
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-24 03:36:11 +0900

    Update Even or Odd.py

--- Practice problem/Even or Odd/Even or Odd.py
+++ Practice problem/Even or Odd/Even or Odd.py
@@ -1,7 +1,7 @@
-# 연습문제 2016 Level 1
+# 연습문제 짝수와 홀수  Level 1
 
-def solution(a, b):
-    month = [ 0, 31, 60, 91, 121, 152, 182,213, 244, 274, 305, 335 ]
-    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
-    answer = day[(month[a - 1] + b) % 7]
+def solution(num):
+    answer = 'Even'
+    if (num % 2 == 1) | (num % 2 == -1) :
+        answer = 'Odd'
     return answer
