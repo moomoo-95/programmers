60f245ccba800e8d01621f5917def56dcd936c0d
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-24 03:31:05 +0900

    base

---
+++ Practice problem/Even or Odd/Even or Odd.py
@@ -0,0 +1,7 @@
+# 연습문제 2016 Level 1
+
+def solution(a, b):
+    month = [ 0, 31, 60, 91, 121, 152, 182,213, 244, 274, 305, 335 ]
+    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    answer = day[(month[a - 1] + b) % 7]
+    return answer
