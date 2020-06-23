36c77ec3bbd7bae7dc17e72a7f6f2376e24e3364
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-24 03:34:01 +0900

    Update Even or Odd.c

--- Practice problem/Even or Odd/Even or Odd.c
+++ Practice problem/Even or Odd/Even or Odd.c
@@ -1,14 +1,15 @@
-// 연습문제 2016 Level 1
+// 연습문제 짝수와  Level 1
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-char* solution(int a, int b) {
-	int month[12] = { 0, 31, 60, 91, 121, 152, 182,213, 244, 274, 305, 335 };
-	char* day[7] = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
-	// 리턴할 값은 메모리를 동적 할당해주세요.
-	char* answer = (char*)malloc(sizeof(char*));
-	answer = day[(month[a - 1] + b) % 7];
-	return answer;
+char* solution(int num) {
+    // 리턴할 값은 메모리를 동적 할당해주세요
+    char* answer = (char*)malloc(4);
+    answer = "Even";
+    if(num % 2 == 1 || num % 2 == -1) {
+        answer = "Odd";
+    }
+    return answer;
 }
