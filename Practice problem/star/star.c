a62c1bb7c5f39d6230fe90b85a77f11593ed43e8
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-26 14:22:21 +0900

    Update star.c

--- Practice problem/star/star.c
+++ Practice problem/star/star.c
@@ -1,15 +1,15 @@
-// 연습문제 짝수와 홀수  Level 1
+// 연습문제 직사각형 별찍기  Level 1
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 
-char* solution(int num) {
-    // 리턴할 값은 메모리를 동적 할당해주세요
-    char* answer = (char*)malloc(4);
-    answer = "Even";
-    if(num % 2 == 1 || num % 2 == -1) {
-        answer = "Odd";
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for(int i = 0; i < b; i++){
+        for(int j = 0; j < a; j++){
+            printf("*");
+        }
+        printf("\n");
     }
-    return answer;
+    return 0;
 }
