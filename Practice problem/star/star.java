0c36b940ecde0f0223b2f29ed6b47536cc11509a
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-26 14:24:44 +0900

    Update star.java

--- Practice problem/star/star.java
+++ Practice problem/star/star.java
@@ -1,11 +1,17 @@
-// 연습문제 짝수와 홀수  Level 1
+// 연습문제 직사각형 별찍기  Level 1
 
-class Solution {
-    public String solution(int num) {
-        String answer = "Even";
-        if(num % 2 == 1 || num % 2 == -1) {
-            answer = "Odd";
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = 0; i < b; i++){
+            for(int j = 0; j < a; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
         }
-        return answer;
     }
 }
