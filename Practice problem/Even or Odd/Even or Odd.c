4e32126e2ae4d4b63ef488c134fe2e986e9c55e4
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-06-29 23:58:04 +0900

    Update Even or Odd.c

--- Practice problem/Even or Odd/Even or Odd.c
+++ Practice problem/Even or Odd/Even or Odd.c
@@ -8,8 +8,6 @@ char* solution(int num) {
     // 리턴할 값은 메모리를 동적 할당해주세요
     char* answer = (char*)malloc(4);
     answer = "Even";
-    if(num % 2 == 1 || num % 2 == -1) {
-        answer = "Odd";
-    }
+    if(num % 2 == 1 || num % 2 == -1) { answer = "Odd"; }
     return answer;
 }
