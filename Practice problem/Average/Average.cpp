a47f0954aa69879aa1a1eed20170239899aefb10
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-05 23:28:40 +0900

    Update Average.cpp

--- Practice problem/Average/Average.cpp
+++ Practice problem/Average/Average.cpp
@@ -6,8 +6,6 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double answer = 0;
-    for(int i = 0; i < arr.size(); i++){
-        answer += arr[i];
-    }
+    for(int i = 0; i < arr.size(); i++){ answer += arr[i]; }
     return answer / arr.size();
 }
