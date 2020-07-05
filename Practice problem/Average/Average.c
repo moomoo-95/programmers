4e3bc468ccff992ea2c6ee4afee15dad82661d2f
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-05 23:28:16 +0900

    Update Average.c

--- Practice problem/Average/Average.c
+++ Practice problem/Average/Average.c
@@ -5,8 +5,6 @@
 // arr_len은 배열 arr의 길이입니다.
 double solution(int arr[], size_t arr_len) {
     double answer = 0;
-    for(int i = 0; i < arr_len; i++){
-        answer += arr[i];
-    }
+    for(int i = 0; i < arr_len; i++){ answer += arr[i]; }
     return answer / arr_len;
 }
