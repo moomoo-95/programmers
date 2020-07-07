7b9f1a77d9758634d661b79a4ffdee7a85a77fdf
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-07 22:42:24 +0900

    A

--- Practice problem/Watermelon/Watermelon.cpp
+++ Practice problem/Watermelon/Watermelon.cpp
@@ -8,12 +8,8 @@ using namespace std;
 string solution(int n) {
     string answer = "";
     for (int i = 0 ; i < n ; i++){
-        if (i % 2 == 0){
-            answer.append("수");
-        }
-        else{
-            answer.append("박");
-        }
+        if (i % 2 == 0){ answer.append("수"); }
+        else{ answer.append("박"); }
     }
     return answer;
 }
