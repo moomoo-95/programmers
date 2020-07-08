a44feafe727c0d79148b5b5ebe86db80ee5c9369
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-08 22:50:00 +0900

    Update star.cpp

--- Practice problem/star/star.cpp
+++ Practice problem/star/star.cpp
@@ -5,13 +5,10 @@
 using namespace std;
 
 int main(void) {
-    int a;
-    int b;
+    int a, b;
     cin >> a >> b;
     for(int i = 0; i < b; i++){
-        for(int j = 0; j < a; j++){
-            printf("*");
-        }
+        for(int j = 0; j < a; j++){ printf("*"); }
         printf("\n");
     }
     return 0;
