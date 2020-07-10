370455a3c04293e1189bd402913e80b6fa73b78b
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-10 23:48:19 +0900

    Update Watermelon.cs

--- Practice problem/Watermelon/Watermelon.cs
+++ Practice problem/Watermelon/Watermelon.cs
@@ -1,13 +1,18 @@
 // 연습문제 수박수박수박수? Level 1
 
-public class Solution {
-    public string solution(int n) {
+public class Solution 
+{
+    public string solution(int n) 
+    {
         string answer = "";
-        for (int i = 0 ; i < n ; i++){
-            if (i % 2 == 0){
+        for (int i = 0 ; i < n ; i++)
+        {
+            if (i % 2 == 0)
+            {
                 answer = answer + "수";
             }
-            else{
+            else
+            {
                 answer = answer + "박";
             }
         }
