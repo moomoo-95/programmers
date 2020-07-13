cd5cf735c18512233bcbb2fc30865ff59fcc8120
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-13 11:41:14 +0900

    Create Harshad number.java

---
+++ Practice problem/Harshad number.java
@@ -0,0 +1,14 @@
+// 하샤드 수
+
+class Solution {
+    public boolean solution(int x) {
+        int z = x;
+        int y = 0;
+        while(x > 0) {
+            y += x%10;
+            x /= 10;
+        }
+        if(z % y == 0){ return true; }
+        return false;
+    }
+}
