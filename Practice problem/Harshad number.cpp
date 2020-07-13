d8abc0905cf22572c9226dd00dd056bfe48a44dd
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-13 11:40:06 +0900

    Create Harshad number.cpp

---
+++ Practice problem/Harshad number.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int z = x;
+    int y = 0;
+    while(x > 0) {
+        y += x%10;
+        x /= 10;
+    }
+    if(z % y == 0){ return true; }
+    return false;
+}
