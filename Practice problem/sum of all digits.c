825d4f283d3085ce16760188bb49eaeb91a8c770
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-17 23:49:32 +0900

    Create sum of all digits.c

---
+++ Practice problem/sum of all digits.c
@@ -0,0 +1,16 @@
+// 자릿수 더하기
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    while(n >= 10){
+        answer += n%10;
+        n -= n%10;
+        n /= 10;
+    }
+    answer += n;
+    return answer;
+}
