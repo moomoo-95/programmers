2c71a0338c0931dc59586095ba0f8028ccc1d7e0
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-18 02:11:46 +0900

    Update Remove the minimum.py

--- Remove the minimum.py
+++ Remove the minimum.py
@@ -1,1 +1,10 @@
 # 연습문제 제일 작은 수 제거하기 Level 1
+
+def solution(arr):
+    answer = []
+    arr.remove(min(arr))
+    if len(arr) == 0:
+        answer.append(-1)
+    else:
+        answer = arr
+    return answer
