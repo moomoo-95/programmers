2dc2c3b1f4a0491b4b68b8c1db7418b55dd71ea0
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-23 00:25:58 +0900

    Update String handling basic.cpp

--- Practice problem/String handling basic.cpp
+++ Practice problem/String handling basic.cpp
@@ -1,2 +1,14 @@
 // 연습문제 문자열 다루기 기본 Level 1
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    if(s.length() != 4 && s.length() !=6){ return false; }
+    for(int i = 0; i < s.length(); i++){
+        if(s.at(i) > 57 || s.at(i) < 48){ return false; }
+    }
+    return true;
+}
