3a01ace882b0578954719659a46b48047ff2a78f
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-23 00:25:35 +0900

    Update String handling basic.java

--- Practice problem/String handling basic.java
+++ Practice problem/String handling basic.java
@@ -1,3 +1,11 @@
 // 연습문제 문자열 다루기 기본 Level 1
 
-
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() != 4 && s.length() !=6){ return false; }
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) > 57 || s.charAt(i) < 48){ return false; }
+        }
+        return true;
+    }
+}
