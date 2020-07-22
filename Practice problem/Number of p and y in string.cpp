c16e3a587e621285b5aa84e6b7820496c4321110
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-22 23:30:03 +0900

    Update Number of p and y in string.cpp

--- Practice problem/Number of p and y in string.cpp
+++ Practice problem/Number of p and y in string.cpp
@@ -1,1 +1,16 @@
-//
+// 연습문제 문자열 내 p와 y의 개수 Level 1
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    int count = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s.at(i) == 'p' || s.at(i) == 'P'){ count++; }
+        else if(s.at(i) == 'y' || s.at(i) == 'Y'){ count--; }
+    }
+    if(count == 0){ return true; }
+    else{ return false; }
+}
