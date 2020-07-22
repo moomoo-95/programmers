fcc840956d77b5cabc1557912ec92eb3894bf45e
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-22 23:30:44 +0900

    Update Number of p and y in string.py

--- Practice problem/Number of p and y in string.py
+++ Practice problem/Number of p and y in string.py
@@ -1,1 +1,13 @@
 # 연습문제 문자열 내 p와 y의 개수 Level 1
+
+def solution(s):
+    count = 0
+    for i in range(len(s)):
+        if(s[i] == 'p' or s[i] == 'P'):
+            count += 1
+        elif(s[i] == 'y' or s[i] == 'Y'):
+            count -= 1
+    if(count == 0):
+        return True
+    else:
+        return False
