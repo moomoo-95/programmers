fcf6994f651ab7a668ca77312c3768126206989d
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-22 23:30:21 +0900

    Update Number of p and y in string.java

--- Practice problem/Number of p and y in string.java
+++ Practice problem/Number of p and y in string.java
@@ -1,1 +1,13 @@
 // 연습문제 문자열 내 p와 y의 개수 Level 1
+
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P'){ count++; }
+            else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y'){ count--; }
+        }
+        if(count == 0){ return true; }
+        else{ return false; }
+    }
+}
