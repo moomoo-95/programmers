2974b191b95cb8c33d00e1b581feba786e20d819
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-23 00:25:07 +0900

    Update String handling basic.py

--- Practice problem/String handling basic.py
+++ Practice problem/String handling basic.py
@@ -1,1 +1,9 @@
-#
+# 연습문제 문자열 다루기 기본 Level 1
+
+def solution(s):
+    if len(s) != 4 and len(s) != 6:
+        return False
+    for i in range(len(s)):
+        if ord(s[i]) > 57 or ord(s[i]) < 48:
+            return False
+    return True
