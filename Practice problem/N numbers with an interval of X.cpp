03e82784758564b67876be43b336fe113a5815fa
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-24 22:31:25 +0900

    Update N numbers with an interval of X.cpp

--- Practice problem/N numbers with an interval of X.cpp
+++ Practice problem/N numbers with an interval of X.cpp
@@ -1,1 +1,12 @@
 // 연습문제 x만큼 간격이 있는 n개의 숫자 Level 1
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    for(int i = 1; i <= n; i++){ answer.push_back(x*i); }
+    return answer;
+}
