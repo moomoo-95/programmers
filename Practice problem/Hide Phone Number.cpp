a95c7871b574b7cdbf5e4d645184a65987aac216
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-26 23:55:20 +0900

    Update Hide Phone Number.cpp

--- Practice problem/Hide Phone Number.cpp
+++ Practice problem/Hide Phone Number.cpp
@@ -1,1 +1,12 @@
-//
+// 연습문제 핸드폰 번호 가리기 Level 1
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    int len = phone_number
+    string answer = "";
+    return answer;
+}
