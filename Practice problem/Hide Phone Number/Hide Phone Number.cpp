f200d6b597de56fc08fd69c68b6e5d34c04f09f1
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-27 00:05:12 +0900

    Update Hide Phone Number.cpp

--- Practice problem/Hide Phone Number/Hide Phone Number.cpp
+++ Practice problem/Hide Phone Number/Hide Phone Number.cpp
@@ -6,7 +6,12 @@
 using namespace std;
 
 string solution(string phone_number) {
-    int len = phone_number
-    string answer = "";
+    int len = phone_number.length();
+    string answer = phone_number.substr(len-4, 4);
+    len -= 4;
+    while (len){
+        answer = "*" + answer;
+        len--;
+    }
     return answer;
 }
