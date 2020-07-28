8d87873dcd2e3c5a2df8d90397ffdbbe9ac2659d
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-28 14:37:14 +0900

    Update Number of 124 countries.py

--- Practice problem/Number of 124 countries.py
+++ Practice problem/Number of 124 countries.py
@@ -1,1 +1,16 @@
-#
+# 연습문제 124 나라의 숫자 Level 1
+
+def solution(n):
+    answer = ''
+    while n > 0:
+        k = n % 3
+        if k == 0:
+            k = 3
+        if k == 1:
+            answer = '1' + answer
+        elif k == 2:
+            answer = '2' + answer
+        else:
+            answer = '4' + answer
+        n = (n- k)/3
+    return answer
