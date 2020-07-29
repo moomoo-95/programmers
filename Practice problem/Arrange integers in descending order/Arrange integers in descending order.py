2d090abf22643117d792fe6a838b0b32b0d8dada
moomoo-95 <58906637+moomoo-95@users.noreply.github.com>
2020-07-29 15:07:24 +0900

    Update Arrange integers in descending order.py

--- Practice problem/Arrange integers in descending order/Arrange integers in descending order.py
+++ Practice problem/Arrange integers in descending order/Arrange integers in descending order.py
@@ -1,1 +1,19 @@
-#
+# 연습문제 정수 내림차순으로 배치하기 Level 1
+
+def solution(n):
+    array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    answer = 0
+    while(1):
+        if n == 0:
+            break
+        array[10] += 1
+        k = (int)(n % 10)
+        array[k] += 1
+        n = (n-k)/10
+    print(array)
+    for i in range(array[10]):
+        while array[array[11]]== 0:
+            array[11] += 1
+        answer = answer + array[11]*(10**i)
+        array[array[11]] -= 1
+    return answer
