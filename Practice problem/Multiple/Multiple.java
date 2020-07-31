e1abc81d67f863d2db20e051eddc82cd9711009c
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-07-31 23:55:03 +0900

    Update Multiple.java

--- Practice problem/Multiple/Multiple.java
+++ Practice problem/Multiple/Multiple.java
@@ -5,16 +5,10 @@ class Solution {
     public int[] solution(int[] arr, int divisor) {
         ArrayList<Integer> answer = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) {
-                answer.add(arr[i]);
-            }
+            if (arr[i] % divisor == 0) { answer.add(arr[i]); }
         }
-        if (answer.isEmpty()) {
-            answer.add(-1);
-        }
-        else {
-            Collections.sort(answer);
-        }
+        if (answer.isEmpty()) { answer.add(-1); }
+        else { Collections.sort(answer); }
         return Arrays.stream(answer.toArray(new Integer[answer.size()])).mapToInt(i->i).toArray();
     }
 }
