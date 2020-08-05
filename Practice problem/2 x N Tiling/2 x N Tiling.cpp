8357b6c3ef71b55e66c672ad16cda2f2e4149fa2
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-05 22:24:02 +0900

    Update 2 x N Tiling.cpp

--- Practice problem/2 x N Tiling/2 x N Tiling.cpp
+++ Practice problem/2 x N Tiling/2 x N Tiling.cpp
@@ -1,1 +1,19 @@
-//
+// 연습문제 2 x n 타일링 Level 3
+
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[60000] = {0};
+int func(int n){
+    if(dp[n-1] == 0){
+        dp[n-1] = (func(n-1)+func(n-2)) % 1000000007;
+    }
+    return dp[n-1];
+}
+int solution(int n) {
+    dp[0] = 1;
+    dp[1] = 2;
+    int answer = func(n);
+    return answer;
+}
