a6eb0c2b482a38802c18e76578584f016dd7751e
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-05 22:24:23 +0900

    Update 2 x N Tiling.java

--- Practice problem/2 x N Tiling/2 x N Tiling.java
+++ Practice problem/2 x N Tiling/2 x N Tiling.java
@@ -1,1 +1,17 @@
-//
+// 연습문제 2 x n 타일링 Level 3
+
+class Solution {
+    int dp[] = new int[60000];
+    int func(int n){
+        if(dp[n-1] == 0){
+            dp[n-1] = (func(n-1)+func(n-2)) % 1000000007;
+        }
+        return dp[n-1];
+    }
+    public int solution(int n) {
+    dp[0] = 1;
+    dp[1] = 2;
+    int answer = func(n);
+        return answer;
+    }
+}
