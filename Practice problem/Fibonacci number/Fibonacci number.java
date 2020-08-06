5f7bb85e69d52fcf77f70d02503227be2172a772
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-06 14:46:40 +0900

    Update Fibonacci number.java

--- Practice problem/Fibonacci number/Fibonacci number.java
+++ Practice problem/Fibonacci number/Fibonacci number.java
@@ -1,1 +1,17 @@
-//
+// 연습문제 피보나치 수열 Level 2
+
+class Solution {
+    int[] farr = new int[100001];
+    public int fib(int n){
+        if(n != 0 && farr[n] == 0){
+            farr[n] = (fib(n-1) + fib(n-2))%1234567;
+        }
+        return farr[n];
+    }
+    public int solution(int n) {
+        farr[0] = 0;
+        farr[1] = 1;
+        int answer = fib(n);
+        return answer;
+    }
+}
