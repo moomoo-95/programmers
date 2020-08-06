38c9f12034321810dcb7974f484833716b69339e
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-06 14:46:17 +0900

    Update Fibonacci number.cpp

--- Practice problem/Fibonacci number/Fibonacci number.cpp
+++ Practice problem/Fibonacci number/Fibonacci number.cpp
@@ -1,1 +1,16 @@
-//
+// 연습문제 피보나치 수열 Level 2
+
+using namespace std;
+int farr[100000] = {0};
+int fib(int n){
+    if(n != 0 && farr[n] == 0){
+        farr[n] = (fib(n-1) + fib(n-2))%1234567;
+    }
+    return farr[n];
+}
+int solution(int n) {
+    farr[0] = 0;
+    farr[1] = 1;
+    int answer = fib(n);
+    return answer;
+}
