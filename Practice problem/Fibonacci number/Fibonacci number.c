13796104d7c63553195442c2a7464f79c0e2de9c
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-06 14:45:57 +0900

    Update Fibonacci number.c

--- Practice problem/Fibonacci number/Fibonacci number.c
+++ Practice problem/Fibonacci number/Fibonacci number.c
@@ -1,1 +1,17 @@
-//
+// 연습문제 피보나치 수열 Level 2
+
+#include <stdio.h>
+
+int farr[100000] = {0};
+int fib(int n){
+    if(n != 0 && farr[n] == 0){
+        farr[n] = (fib(n-1) + fib(n-2))%1234567;
+    }
+    return farr[n];
+}
+int solution(int n) {
+    farr[0] = 0;
+    farr[1] = 1;
+    int answer = fib(n);
+    return answer;
+}
