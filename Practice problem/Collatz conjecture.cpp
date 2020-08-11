bc545681f6ddda66b56bd27b11a8c8895c55bde2
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-12 01:44:37 +0900

    Create Collatz conjecture.cpp

---
+++ Practice problem/Collatz conjecture.cpp
@@ -0,0 +1,21 @@
+//
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long N = num;
+    int answer = 0;
+    while(N != 1){
+        if(N % 2 == 0){N /= 2;}
+        else{N = N*3 + 1;}
+        answer++;
+        if(answer == 500) {
+            answer = -1;
+            break;
+        }
+    }
+    return answer;
+}
