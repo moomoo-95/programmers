c898fe740e8281a7199dd80acd9483d260bc5721
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-14 00:35:30 +0900

    Create multiplication of matrix.cpp

---
+++ Practice problem/multiplication of matrix.cpp
@@ -0,0 +1,21 @@
+//
+
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for(int i = 0; i < arr1.size(); i++ ){
+        vector<int> temp;
+        for(int j = 0; j < arr2[0].size(); j++){
+            int temp2 = 0;
+            for(int k = 0; k < arr1[0].size(); k++){
+                temp2 += arr1[i][k]*arr2[k][j];
+            }
+            temp.push_back(temp2);
+        }
+        answer.push_back(temp);
+    }
+    return answer;
+}
