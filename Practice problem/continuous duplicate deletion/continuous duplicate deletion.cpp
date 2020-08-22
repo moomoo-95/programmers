03037c1e6dd6036042dab12a6b57ab3b5dd747de
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-08-22 23:57:03 +0900

    Update continuous duplicate deletion.cpp

--- Practice problem/continuous duplicate deletion/continuous duplicate deletion.cpp
+++ Practice problem/continuous duplicate deletion/continuous duplicate deletion.cpp
@@ -10,9 +10,7 @@ vector<int> solution(vector<int> arr)
 	vector<int> answer;
 	answer.push_back(arr[0]);
 	for (int i = 1; i < arr.size(); i++) {
-		if (answer[answer.size() - 1] != arr[i]) {
-			answer.push_back(arr[i]);
-		}
+		if (answer[answer.size() - 1] != arr[i]) { answer.push_back(arr[i]); }
 	}
 	return answer;
 }
