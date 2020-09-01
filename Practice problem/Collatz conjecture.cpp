98ab73b720d8fbd602f2fafa9e0cb6119c222f61
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-09-01 23:58:37 +0900

    Update Collatz conjecture.cpp

--- Practice problem/Collatz conjecture.cpp
+++ Practice problem/Collatz conjecture.cpp
@@ -6,13 +6,20 @@ int solution(int num) {
     long long N = num;
     int answer = 0;
     while(N != 1){
-        if(N % 2 == 0){N /= 2;}
-        else{N = N*3 + 1;}
+        if(N % 2 == 0){
+            N /= 2;
+        }
+        else{
+            N = N*3 + 1;
+        }
+        
         answer++;
+        
         if(answer == 500) {
             answer = -1;
             break;
         }
     }
+    
     return answer;
 }
