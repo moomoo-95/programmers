0d722a4034f8311a47b3328baa56391f4d4332a0
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2020-09-07 23:16:49 +0900

    Update Carpet.cpp

--- Brute-force Search/Carpet.cpp
+++ Brute-force Search/Carpet.cpp
@@ -9,12 +9,14 @@ vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int i = 1;
     int temp = yellow;
+    
     while(answer.empty()){
         if(brown == temp*2 + (i+2)*2){
             answer.push_back(temp+2);
             answer.push_back(i+2);
             break;
         }
+        
         while(yellow % (i+1) != 0){i++;}
         i++;
         temp = yellow / i;
