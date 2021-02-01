2b454be91ed76efd77d28c3c503bf57b70e8c0a9
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2021-02-01 18:02:40 +0900

    Create more spicier.py

---
+++ Heap/more spicier.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heap = []
+    
+    for i in range(len(scoville)):
+        heapq.heappush(heap, scoville[i])
+        
+    temp = heapq.heappop(heap)
+    
+    while (temp < K and len(heap)!=0):
+        temp += heapq.heappop(heap)*2
+        heapq.heappush(heap, temp)
+        temp = heapq.heappop(heap)
+        answer += 1
+    if(temp < K):
+        answer = -1
+    return answer
