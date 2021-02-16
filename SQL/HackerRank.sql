8926ee7c4bf29bbaf5b1565740e6ee1b8582434a
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2021-02-16 17:22:49 +0900

    Update HackerRank.sql

--- SQL/HackerRank.sql
+++ SQL/HackerRank.sql
@@ -1,1 +1,20 @@
+-- Revising the Select Query I
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = "USA";
 
+-- Select All
+ELECT * FROM CITY;
+
+-- Select By ID
+SELECT * FROM CITY WHERE ID = 1661;
+
+-- Japanese Cities' Attributes
+SELECT * FROM CITY WHERE COUNTRYCODE = "JPN";
+
+-- Japanese Cities' Names
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "JPN";
+
+-- Weather Observation Station 1
+SELECT CITY, STATE FROM STATION;
+
+-- Weather Observation Station 3
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0;
