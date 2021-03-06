a2d05623a3cd3ceccbc4b03f4d29f4a807ac8a43
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2021-03-06 23:59:38 +0900

    Update Press Keypad.py

--- kakao/Press Keypad.py
+++ kakao/Press Keypad.py
@@ -1,1 +1,28 @@
 # 2020카카오 인턴쉽 키패드 누르기
+
+def solution(numbers, hand):
+    answer = ''
+    ll, rl = 10, 12
+    for v in numbers:
+        if v in [1, 4, 7]:
+            answer += 'L'
+            ll = v
+        elif v in [3, 6, 9]:
+            answer += 'R'
+            rl = v
+        else:
+            t = 11 if v == 0 else v
+            if abs(ll-t)//3 + abs(ll-t)%3 < abs(rl-t)//3 + abs(rl-t)%3:
+                answer += 'L'
+                ll = t
+            elif abs(ll-t)//3 + abs(ll-t)%3 > abs(rl-t)//3 + abs(rl-t)%3:
+                answer += 'R'
+                rl = t
+            else:
+                if hand == "left":
+                    answer += 'L'
+                    ll = t
+                else:
+                    answer += 'R'
+                    rl = t
+    return answer
