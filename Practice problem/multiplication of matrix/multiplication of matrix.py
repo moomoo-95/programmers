908cb4633581b0a0e28ae9437534128c483c43ce
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2021-03-07 23:41:23 +0900

    Create multiplication of matrix.py

---
+++ Practice problem/multiplication of matrix/multiplication of matrix.py
@@ -0,0 +1,12 @@
+
+def solution(arr1, arr2):
+    answer = []
+    for i in  range(len(arr1)):
+        tmpl = []
+        for j in range(len(arr2[0])):
+            tmpi = 0
+            for k in range(len(arr1[0])):
+                tmpi += arr1[i][k]*arr2[k][j]
+            tmpl.append(tmpi)
+        answer.append(tmpl)
+    return answer
