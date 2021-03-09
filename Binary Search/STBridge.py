5fc4ef64f107a500273a47db6970747f6dedb5b4
HyeonSeong Lim <58906637+moomoo-95@users.noreply.github.com>
2021-03-09 23:50:39 +0900

    Create STBridge.py

---
+++ Binary Search/STBridge.py
@@ -0,0 +1,22 @@
+# 이분탐색 level 3 징검다리
+
+def solution(distance, rocks, n):
+    answer, left, right = 0, 1, distance
+    rocks.sort()
+    rocks.append(distance)
+    while left < right:
+        middle = (left+right)//2
+        mini = middle
+        start, dist = 0, 0
+        for v in rocks:
+            if v-start < middle:
+                dist += 1
+            else:
+                mini = min(mini, v-start)
+                start = v
+        if dist > n:
+            right = middle
+        else:
+            answer = mini
+            left = middle +1
+    return answer
